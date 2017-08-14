fe10280ef2c32fcd1edef6f3be9b40b17f599317
Andreas <AndreasPapaz@gmail.com>
2017-08-14 12:24:37 -0500

    adding videos

--- src/index.js
+++ src/index.js
@@ -4,14 +4,15 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyDyKCQW0Nu39ns1zPW76z3nzMUr1HPxcms';
 
 
-//example
-YTSearch({key: API_KEY, term: 'surf'}, function(data) {
-	console.log(data);
-});
+// //example
+// YTSearch({key: API_KEY, term: 'surf'}, function(data) {
+// 	console.log(data);
+// });
 
 class App extends Component {
 
@@ -22,12 +23,11 @@ class App extends Component {
 			videos: []
 		};
 
-		YTSearch({key: API_KEY, term: 'surf'}, (videos) => {
+		YTSearch({key: API_KEY, term: 'dogs'}, (videos) => {
 			this.setState({
 				videos
 			})
 		});
-
 	}
 
 	render() {
@@ -35,6 +35,7 @@ class App extends Component {
 	return (
 		<div>
 			<SearchBar />
+			<VideoDetail video={this.state.videos[0]} />
 			<VideoList videos={this.state.videos} />
 		</div>
 	);
